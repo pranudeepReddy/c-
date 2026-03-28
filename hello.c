02042156544f6819ea04e806f1abcdc2d0bc847b
pranudeepReddy <pranudeep2001@gmail.com>
2026-03-28 15:01:12 +0530

    Inital commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ main(){
+ //printf("hello\n");
+ //printf("worlds");
+ //printf("hellosss\r");
+ //printf("world");
+ //printf("hello\b");
+ //printf("worlds");
+ //printf("hello\t");
+ //printf("worlds");
+ printf("hello\a");
+ printf("worlds");
+ //printf("hello");
+ //printf("worlds");
+ //printf("hello");
+ //printf("worlds");
+ 
+ return 0;
+ }
